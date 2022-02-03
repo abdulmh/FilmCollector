6e0f862f5b5f00439924b14352d0eb943fcd34e9
abdulmh <abdul-malikh@chc.edu>
2022-02-03 10:42:00 -0500

    Started Create Actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,22 @@
 class ActorsController < ApplicationController
+	# index method gets called when the actors URL is requested 
+	# index method gets mapped to the actors index.html.erb.
 	def index 
+		# call all method on Actor model class.
+		# all method selects all of the data in the actor table
+		# and returns the data as an array of objects.
+		# store the array of objects in an instance variable.
+		# instance variable is avialable to actors index.html.erb.
 		@actors = Actor.all
+	end
+	
+	# new method gets called when the actors new URL is requested.
+	# new nethod is mapped to the actors new.html.erb
+	def new
+	end
+
+	# create method gets called when the Create buttion is pushed on
+	# the actors new.html.erb.
+	def create
 	end
 end
