cf201bde0cc539090b36b54eb6c15e3d037e9d39
abdulmh <abdul-malikh@chc.edu>
2022-02-08 10:57:14 -0800

    Completed Create Actor and started View Movies

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -18,5 +18,34 @@ class ActorsController < ApplicationController
 	# create method gets called when the Create buttion is pushed on
 	# the actors new.html.erb.
 	def create
+		# call constructor of Actor model class giving it the
+		# first name and last name parameters input in the actors
+		# new.html.erb
+		# constructo creates Actor model object which is stored
+		# in variable
+		actor = Actor.new(actor_params)
+		# call save method on Acotr object
+		# save metod inserts the data in the Actor model object
+		# into the acotr table
+		if actor.save
+			# if the save method succeds, request the actors URL
+			# which will rendor the actors index.html.erb in the browser
+			redirect_to "/actors"
+		else
+			# get full messaes associated with errors
+			# store them in a Rails flahs object named errors so that
+			# the full message may be displayed in the actors new.html.erb
+			flash[:errors] = actor.errors.full_messages
+			# if the save method succeds, request the actors/new URL
+			# which will rendor the actors new.html.erb in the browser
+			redirect_to "/actors/new"
+		end 
+	end
+
+	private
+	def actor_params
+		# params in a Rails object that gets the specified request
+		# parameters 
+		params.require(:actor).permit(:first_name, :last_name)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   # When a post request is made for the actors URL,
   # Rails envokes the create method in the actors Ruby controller class
   post "actors" => "actors#create"
+  get "movies" => "movies#index"
+  get "movies/new" => "movies#new"
 end
