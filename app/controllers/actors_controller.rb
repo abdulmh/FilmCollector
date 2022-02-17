1c74194f201055a483722bd04c27a3d3a533e2fe
abdulmh <abdul-malikh@chc.edu>
2022-02-17 10:48:56 -0800

    Completed the Edit Actor page and started the Delete Actor page

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -36,14 +36,14 @@ class ActorsController < ApplicationController
 			# store them in a Rails flash object named errors so that
 			# the full message may be displayed in the actors new.html.erb
 			flash[:errors] = actor.errors.full_messages
-			# if the save method succeds, request the actors/new URL
+			# if the save method succeeds, request the actors/new URL
 			# which will rendor the actors new.html.erb in the browser
 			redirect_to "/actors/new"
 		end 
 	end
 
 	# show method gets called when the actors/:id URL is requested
-	#show method is mapped to the actors show.html.erb
+	# show method is mapped to the actors show.html.erb
 	def show
 		# call find method on Actor model class giving it the id sent
 		# in the request
@@ -52,6 +52,71 @@ class ActorsController < ApplicationController
 		# store the array of actor objects in an instance variable 
 		# instance variable is available to actors show.html.erb
 		@movies = Actor.find(params[:id]).movies
+	end
+
+	# edit metod gets called when the actors/:id/edit URL is requested
+	# edit method is mapped out to the actors edit.html.erb
+	def edit
+		# call find method on Actor model class giving it the id sent
+		# in the request
+		# the find method selects all of the data in the actor table where
+		# the id is equal to the id sent in the request
+		# the selected data will be returned as an object
+		# the object will be stored in an instance variable 
+		# that will be available to the edit.html.erb 
+		@actor = Actor.find(params[:id])
+	end
+
+	# update method gets called when the update button is pushed on the 
+	# actors edit.html.erb
+	def update
+
+		# call find method on Actor model clss givng it the id sent in the 
+		# request
+		# find method selects all of the data in the actor table where
+		# the id is equal to the id sent in the request
+		# the slected data will be returned as an object
+		# the object will be stored in a variable
+		actor = Actor.find(params[:id])
+		# call update method on Actor object giving it the first name and
+		# last name parameters input in the actors edit.html.erb
+		# update method updates the data in the actor table use the parameters
+		if actor.update(actor_params)
+			# if the updae method succeeds, request the actors URL which
+			# will render the actors index.html.erb in the browser
+			redirect_to "/actors"
+		else
+			# if the update fails, get full messages associated 
+			# with errors
+			# store them in a Rails flash object named errors so that
+			# the full message may be displayed in the requested URL
+			flash[:errors] = actor.errors.full_messages
+			# request the actors/:id/edit URL which will render actors
+			# edit.html.erb 
+			redirect_to "/actors/#{actor.id}/edit"
+	end
+end
+	
+	# delete method get cslled when the actors/:id/delete URL is requested
+	# delete method is mapped to the actors delete.html.erb
+	def delete
+
+		# call find method on Actor model class giving it the id sent
+		# in the request
+		# the find method selects all of the data in the actor table where
+		# the id is equal to the id sent in the request
+		# the selected data will be returned as an object
+		# the object will be stored in an instance variable 
+		# that will be available to the delete.html.erb 
+		@actor = Actor.find(params[:id])
+	end
+
+	# destroy metod gets called when the Delete button is pushed on the 
+	# actors delete.html.erb
+	def destroy
+		actor = Actor.find(params[:id])
+		actor.destroy
+		redirect_to "/actors"
 	end
 
 	private
